f13e9c19b57725a9991ede795fc0022fd2cd1aab
rajasulaimanshah92 <rajasulaimanshah92@gmail.com>
2019-04-18 15:35:05 +0800

    Init

---
+++ Javascript/test.js
@@ -0,0 +1,15 @@
+console.log('Test');
+
+
+//untuk tukar content dan styles
+function myDate() {
+  document.getElementById("date").innerHTML = Date();
+  document.getElementById("date").style.backgroundColor = "darkseagreen";
+  document.getElementById("date").style.fontSize = "25px";
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+  document.getElementById("demo").style.backgroundColor = "yellow";
+  document.getElementById("demo").style.fontSize = "25px";
+}
